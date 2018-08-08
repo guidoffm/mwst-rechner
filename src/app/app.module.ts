15288109c7dcf03895cbe37ca61a7b8cb0c24719
Guido Mueller <guido.mueller@outlook.de>
2018-08-08 13:39:58 +0200

    localeDe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -9,7 +9,10 @@ import { HomePage } from '../pages/home/home';
 import { BruttoZuNetto } from '../pages/home/brutto-zu-netto/brutto-zu-netto';
 import { NettoZuBrutto } from '../pages/home/netto-zu-brutto/netto-zu-brutto';
 import { StSatzWahl } from '../pages/home/st-satz-wahl/st-satz-wahl';
-import { CurrencyPipe } from '../../node_modules/@angular/common';
+import { CurrencyPipe, registerLocaleData } from '../../node_modules/@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
   declarations: [
@@ -35,6 +38,7 @@ import { CurrencyPipe } from '../../node_modules/@angular/common';
     StatusBar,
     SplashScreen,
     CurrencyPipe,
+    { provide: LOCALE_ID, useValue: 'de' },
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
